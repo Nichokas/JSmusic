b8ee2b69f9d501bd1639d77ef31a957e3b11a4e2
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-02-20 19:46:09 +0100

    errror caching added

--- src/play.js
+++ src/play.js
@@ -1,25 +1,32 @@
+const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const Speaker = require('speaker');
-const fs = require('fs');
 
 function play(audioPlayer) {
-    if (audioPlayer.isPlaying) return;
+    // Verificar si el archivo existe antes de intentar reproducirlo
+    fs.access(audioPlayer.filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error(`Error: No se pudo encontrar el archivo especificado: ${audioPlayer.filePath}`);
+            return; // Salir de la función si el archivo no existe
+        }
 
-    audioPlayer.stream = fs.createReadStream(audioPlayer.filePath);
-    audioPlayer.speaker = new Speaker();
+        // Si el archivo existe, proceder con la reproducción
+        audioPlayer.stream = fs.createReadStream(audioPlayer.filePath);
+        audioPlayer.speaker = new Speaker();
 
-    ffmpeg(audioPlayer.stream)
-        .audioCodec('pcm_s16le')
-        .format('wav')
-        .pipe(audioPlayer.speaker);
+        ffmpeg(audioPlayer.stream)
+            .audioCodec('pcm_s16le')
+            .format('wav')
+            .pipe(audioPlayer.speaker);
 
-    audioPlayer.speaker.on('open', () => {
-        audioPlayer.isPlaying = true;
-        audioPlayer.startTime = Date.now();
-    });
+        audioPlayer.speaker.on('open', () => {
+            audioPlayer.isPlaying = true;
+            audioPlayer.startTime = Date.now();
+        });
 
-    audioPlayer.speaker.on('close', () => {
-        audioPlayer.isPlaying = false;
+        audioPlayer.speaker.on('close', () => {
+            audioPlayer.isPlaying = false;
+        });
     });
 }
 
